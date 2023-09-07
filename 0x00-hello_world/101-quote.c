25323b06bbf8351905acbb048d1e1725b87910b1
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-07 16:44:58 -0700

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Failure)
+ */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 59);
-    return 1;
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 59);
+	return (1);
 }
