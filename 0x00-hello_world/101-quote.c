110a1fdb13b76afde884b393438448457f9bda25
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-07 16:41:21 -0700

    Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, 59);
+    return 1;
+}
