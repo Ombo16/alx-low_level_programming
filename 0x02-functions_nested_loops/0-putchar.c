40f6c827dc638d3ca2a33a5b35ac1e6c600ef194
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-12 13:08:55 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,28 +10,28 @@
  */
 int main(void)
 {
-    char putchar_char;
-    char newline_char;
+	char putchar_char;
+	char newline_char;
 
-    putchar_char = '_';
-    write(1, &putchar_char, 1);  /* Using write to print a single character */
-    putchar_char = 'p';
-    write(1, &putchar_char, 1);
-    putchar_char = 'u';
-    write(1, &putchar_char, 1);
-    putchar_char = 't';
-    write(1, &putchar_char, 1);
-    putchar_char = 'c';
-    write(1, &putchar_char, 1);
-    putchar_char = 'h';
-    write(1, &putchar_char, 1);
-    putchar_char = 'a';
-    write(1, &putchar_char, 1);
-    putchar_char = 'r';
-    write(1, &putchar_char, 1);
+	putchar_char = '_';
+	write(1, &putchar_char, 1);  /* Using write to print a single character */
+	putchar_char = 'p';
+	write(1, &putchar_char, 1);
+	putchar_char = 'u';
+	write(1, &putchar_char, 1);
+	putchar_char = 't';
+	write(1, &putchar_char, 1);
+	putchar_char = 'c';
+	write(1, &putchar_char, 1);
+	putchar_char = 'h';
+	write(1, &putchar_char, 1);
+	putchar_char = 'a';
+	write(1, &putchar_char, 1);
+	putchar_char = 'r';
+	write(1, &putchar_char, 1);
 
-    newline_char = '\n';
-    write(1, &newline_char, 1);  /* Printing a new line */
+	newline_char = '\n';
+	write(1, &newline_char, 1);  /* Printing a new line */
 
-    return (0);
+	return (0);
 }
