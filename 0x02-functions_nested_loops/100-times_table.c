87908afe2c1af260074eebd9b28cc95a4e5c68bd
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-13 12:25:14 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,36 +6,31 @@
  */
 void print_times_table(int n)
 {
-	int i, j, result;
-
 	if (n < 0 || n > 15)
 		return;
 
+	int i, j, result;
+
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		_putchar('0');  /* Print the first digit (always 0) */
+		for (j = 1; j <= n; j++)
 		{
 			result = i * j;
-			if (j == 0)
-				_putchar('0' + result);
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
-				if (result < 10)
-					_putchar(' ');
-				else if (result >= 10 && result < 100)
-					_putchar('0' + (result / 10));
+			if (result < 10)
+				_putchar(' '); /* Extra space for single-digit numbers */
+			if (result>=10 && result < 100)
+				_putchar(' '); /* Extra space for double-digit numbers */
+			if (result >=100 && result <1000)
+				_putchar(' ')
 
-				_putchar('0' + (result % 10));
-				else if (result > 100)
-				_putchar(' ');
-
+			/* Print the result */
 			_putchar('0' + (result / 100));
 			_putchar('0' + ((result / 10) % 10));
 			_putchar('0' + (result % 10));
-			}
 		}
 		_putchar('\n');
 	}
