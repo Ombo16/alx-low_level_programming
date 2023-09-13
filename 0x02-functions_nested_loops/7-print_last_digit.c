08871896fe41570b36bd3052ce121970b156d34e
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-12 17:07:15 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,13 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-
-	last_digit = n % 10;
+if (n == INT_MIN)
+    { 
+	    last_digit = 8;
+    }
+else 
+	{	last_digit = n % 10;
+}
 
 	_putchar(last_digit + '0');
 
