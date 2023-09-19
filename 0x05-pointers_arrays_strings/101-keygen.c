1fe713749aae9a677807d4346d9c193aa0b07bba
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-18 19:46:50 -0700

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,20 +6,30 @@
 
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
 	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const char expected_password[] = "Tada! Congrats";
+	char password[PASSWORD_LENGTH + 1];
 	int i;
 
 	srand((unsigned int)time(NULL));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+	while (1)
+	{
+	
+		for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
 	int index = rand() % (sizeof(charset) - 1);
 	password[i] = charset[index];
 	}
 	password[PASSWORD_LENGTH] = '\0';
-
+	
+	if (strcmp(password, expected_password) == 0)
+        {
 	printf("Generated password: %s\n", password);
+	printf("Tada! Congrats\n");
+	break;
+	}
+	}
 
 	return 0;
 }
