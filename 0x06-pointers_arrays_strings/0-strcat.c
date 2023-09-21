3d125316c2e0e0b6b629dc3754f2d2b7c3b03113
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-21 14:19:04 -0700

    Pointers, Arrays, and Strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,19 @@
  * Return: A pointer to the resulting string `dest`.
  */
 
-char *_strcat(char *dest, char *src) 
-
+char *_strcat(char *dest, char *src)
 {
+	int a, b;
 
-  int i = 0;
-  while (dest[i] != '\0') {
-    i++;
-  }
-  int j = 0;
-  while (src[j] != '\0') {
-    dest[i] = src[j];
-    i++;
-    j++;
-  }
-  dest[i] = '\0';
-  return dest;
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+
+	for (aj = 0; src[b] != '\0'; b++)
+	{
+		dest[a] = src[b];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
 }
