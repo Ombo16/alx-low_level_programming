1719ecce261d61c874450d4a053021979385e688
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-21 14:21:48 -0700

    Pointers, Arrays, and Strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 	for (a = 0; dest[a] != '\0'; a++)
 		;
 
-	for (aj = 0; src[b] != '\0'; b++)
+	for (b = 0; src[b] != '\0'; b++)
 	{
 		dest[a] = src[b];
 		a++;
