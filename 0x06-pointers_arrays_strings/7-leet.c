226d56c73599d644315f774d493b129a45643bda
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-22 02:42:40 -0700

    Pointers, Arrays, and Strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The string to encode.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *leet(char *str)
+{
+	char *result = str;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_replacements = "4433007711";
+
+	while (*str)
+	{
+	char *leet_char = leet_chars;
+	char *replacement = leet_replacements;
+	int replace = 0;
+
+	while (*leet_char)
+	{
+	if (*str == *leet_char)
+		{
+		*str = *replacement;
+		replace = 1;
+		break;
+		}
+
+	leet_char++;
+	replacement++;
+	}
+
+	if (!replace)
+        {
+	str++;
+	}
+	}
+
+	return (result);
+}
