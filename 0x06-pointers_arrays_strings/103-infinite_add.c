b6da3f65033fbcaa677061f924b500b36b6edb2c
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-22 13:20:38 -0700

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,42 +13,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, carry = 0;
-    int sum, index;
+	int len1 = 0, len2 = 0, carry = 0;
+	int sum, index;
 
-    while (n1[len1])
-        len1++;
-    while (n2[len2])
-        len2++;
+	while (n1[len1])
+	len1++;
+	while (n2[len2])
+	len2++;
 
-    if (size_r <= len1 || size_r <= len2)
-        return (0);
+	if (size_r <= len1 || size_r <= len2)
+	return (0);
 
+	len1--;
+	len2--;
+	r[size_r] = '\0';
+	index = size_r - 1;
 
-    len1--;
-    len2--;
-    r[size_r] = '\0';
-    index = size_r - 1;
+	while (len1 >= 0 || len2 >= 0 || carry)
+	{
+	int num1 = (len1 >= 0) ? (n1[len1] - '0') : 0;
+ 	int num2 = (len2 >= 0) ? (n2[len2] - '0') : 0;
 
-    while (len1 >= 0 || len2 >= 0 || carry)
-    {
-        int num1 = (len1 >= 0) ? (n1[len1] - '0') : 0;
-        int num2 = (len2 >= 0) ? (n2[len2] - '0') : 0;
+  	sum = num1 + num2 + carry;
+  	carry = sum / 10;
+ 	r[index] = (sum % 10) + '0';
 
-        sum = num1 + num2 + carry;
-        carry = sum / 10;
-        r[index] = (sum % 10) + '0';
+ 	len1--;
+ 	len2--;
+ 	index--;
+	}
 
-        len1--;
-        len2--;
-        index--;
-    }
+	if (index < 0)
+	return (0);
 
-    if (index < 0)
-        return (0);
+	if (index > 0)
+	r = r + index + 1;
 
-    if (index > 0)
-        r = r + index + 1;
-
-    return r;
+	return r;
 }
