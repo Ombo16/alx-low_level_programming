d17bff314aeacf004703c274e98ec00234211a8d
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-22 12:42:46 -0700

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,31 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to print.
+ * print_number - Print an integer using only _putchar
+ * @n: The integer to print
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');  // Print '-' for negative numbers
-        n = -n;         // Make n positive for processing
-    }
+	int (divisor, neg_flag, digit);
 
-    if (n / 10 != 0)
-    {
-        print_number(n / 10);  // Recursive call to print higher-order digits
-    }
+	divisor = 1;
+	neg_flag = 0;
+	digit = (n/divisor)%10;
 
-    _putchar((n % 10) + '0');  // Print the last digit as a character
+	if (n < 0)
+	{
+		_putchar('-');
+		neg_flag = 1;
+	}
+
+	while (n / divisor > 9 || n / divisor < -9)
+		divisor *= 10;
+
+	while (divisor > 0)
+	{
+		if (digit < 0)
+			digit = -digit;
+		_putchar(digit + '0');
+		divisor /= 10;
+	}
 }
