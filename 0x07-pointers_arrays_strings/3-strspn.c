80c49cb9e907f61ef7bc181baac65a57646e2d18
Ombo16 <ombo.mcboaz@gmail.com>
2023-09-25 16:31:00 -0700

    Pointers, Arrays and Strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,28 +10,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i, j;
-    int found;
+	unsigned int count = 0;
+	int i, j;
+	int found;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        found = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	found = 0;
 
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                found = 1;
-                break;
-            }
-        }
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+			{
+			found = 1;
+			break;
+			}
+	}
 
-        if (!found)
-            return count;
+ 	if (!found)
+	return count;
 
-        count++;
-    }
+	count++;
+	}
 
-    return (count);
+	return (count);
 }
