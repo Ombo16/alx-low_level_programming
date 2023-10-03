1fc9813b48f9d4bae78a773be1bd535367d14a39
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-03 15:02:57 -0700

    Memory Allocation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,70 +3,70 @@
 #include <string.h>
 
 /**
+ * count_words - takes an input string str and
+ * counts the number of words in the string
  * strtow - the function splits a string to words.
- * @str: the string to work on it.
- * Return: a pointer of the splited words.
+ * @str: The input string to count words in
+ * Return: The pointer to a new string or NULL if not found
  */
-int count_words(char *str);
+int count_words(char *str) {
+	int count = 0;
+	int in_word = 0;
+	int a;
 
-char **strtow(char *str) {
-    int num_words;
-    char **words;
-    int word_index;
-    int word_length;
-    int in_word;
-    int a;
+	for (a = 0; str[a] != '\0'; a++) {
+		if (str[a] != ' ') {
+			if (!in_word) {
+				count++;
+				in_word = 1;
+			}
+		} else {
+			in_word = 0;
+		}
+	}
 
-    if (str == NULL || *str == '\0') {
-        return NULL;
-    }
-
-    num_words = count_words(str);
-    words = (char **)malloc((num_words + 1) * sizeof(char *));
-    if (words == NULL) {
-        return NULL;
-    }
-
-    word_index = 0;
-    word_length = 0;
-    in_word = 0;
-
-    for (a = 0; str[a] != '\0'; a++) {
-        if (str[a] != ' ') {
-            if (!in_word) {
-                in_word = 1;
-                word_length = 1;
-                words[word_index] = &str[a];
-                word_index++;
-            } else {
-                word_length++;
-            }
-        } else if (in_word) {
-            str[a] = '\0';
-            in_word = 0;
-        }
-    }
-
-    words[num_words] = NULL;
-
-    return words;
+	return count;
 }
 
-int count_words(char *str) {
-    int count = 0;
-    int in_word = 0;
-    int a;
+char **strtow(char *str) {
+	int num_words;
+	char **words;
+	int word_index;
+	int word_length;
+	int in_word;
+	int a;
 
-    for (a = 0; str[a] != '\0'; a++) {
-        if (str[a] != ' ') {
-            if (!in_word) {
-                count++;
-                in_word = 1;
-            }
-        } else {
-            in_word = 0;
-        }
-    }
+	if (str == NULL || *str == '\0') {
+		return NULL;
+	}
 
-    return count;
+	num_words = count_words(str);
+	words = (char **)malloc((num_words + 1) * sizeof(char *));
+	if (words == NULL) {
+		return NULL;
+	}
+
+	word_index = 0;
+	word_length = 0;
+	in_word = 0;
+
+	for (a = 0; str[a] != '\0'; a++) {
+		if (str[a] != ' ') {
+			if (!in_word) {
+				in_word = 1;
+				word_length = 1;
+				words[word_index] = &str[a];
+				word_index++;
+			} else {
+				word_length++;
+			}
+		} else if (in_word) {
+			str[a] = '\0';
+			in_word = 0;
+		}
+	}
+
+	words[num_words] = NULL;
+
+	return words;
 }
