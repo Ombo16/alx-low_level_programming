6cca0dfe0d13d31fddfb131a0dfad83bb33e707d
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-03 14:44:46 -0700

    Memory Allocation

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - the function splits a string to words.
+ * @str: the string to work on it.
+ * Return: a pointer of the splited words.
+ */
+int count_words(char *str) 
+{
+	int count = 0;
+	int in_word = 0;
+
+	for (int a = 0; str[a] != '\0'; a++) 
+	{
+		if (str[a] != ' ') 
+		{
+			if (!in_word) 
+			{
+				count++;
+				in_word = 1;
+			}
+		} 
+		else 
+		{
+			in_word = 0;
+		}
+	}
+
+	return (count);
+}
+
+char **strtow(char *str) 
+{
+	if (str == NULL || *str == '\0') 
+	{
+		return (NULL);
+	}
+
+	int num_words = count_words(str);
+	char **words = (char **)malloc((num_words + 1) * sizeof(char *));
+	if (words == NULL) 
+	{
+		return (NULL);
+	}
+
+	int word_index = 0;
+	int word_length = 0;
+	int in_word = 0;
+
+	for (int a = 0; str[a] != '\0'; a++) 
+	{
+		if (str[a] != ' ') 
+		{
+			if (!in_word) 
+			{
+				in_word = 1;
+				word_length = 1;
+				words[word_index] = &str[a];
+				word_index++;
+			} 
+			else 
+			{
+				word_length++;
+			}
+		} 
+		else if (in_word) 
+		{
+			str[a] = '\0';
+			in_word = 0;
+		}
+	}
+
+	words[num_words] = NULL;
+
+	return (words);
+}
+
