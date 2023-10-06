96341103143684697050671fe5cb05778341be49
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-06 13:25:50 -0700

    More Malloc Free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments received.
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: if not receive 2 arg, rt error
+ */
+int main(int argc, char *argv[])
+{
+    int n1, n2, mul;
+
+    if (argc != 3)
+        printf("Error\n");
+    else
+    {
+        if (sscanf(argv[1], "%d", &n1) != 1 || sscanf(argv[2], "%d", &n2) != 1)
+        {
+            printf("Error\n");
+            return 98;
+        }
+        mul = n1 * n2;
+        printf("%d\n", mul);
+    }
+    return (0);
+}
