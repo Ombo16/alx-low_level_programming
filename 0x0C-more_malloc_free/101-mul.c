e1d8d9ced83c41c8fd45d6220cd6ad4b2fe9ac4c
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-06 13:49:34 -0700

    More Malloc Free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,46 +1,25 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
-int multiply(int num1, int num2)
-{
-	return (num1 * num2);
-}
-
-int is_positive_number(const char *str)
-{
-	if (str == NULL || *str == '\0')
-		return (0);
-
-	while (*str)
-	{
-		if (!isdigit(*str))
-			return (0);
-		str++;
-	}
-
-	return (1);
-}
-
+/**
+ * main - prints all arguments received.
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: if not receive 2 arg, rt error
+ */
 int main(int argc, char *argv[])
 {
+	int num1, num2, mul;
+
 	if (argc != 3)
-	{
 		printf("Error\n");
-		return (98);
-	}
-
-	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+	else
 	{
-		printf("Error\n");
-		return (98);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
 	}
-
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	int result = multiply(num1, num2);
-	printf("%d\n", result);
-
 	return (0);
 }
