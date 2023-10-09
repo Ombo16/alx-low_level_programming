67d25f667dc7f30341e9ee9fb08af0087d3d16f6
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-09 13:41:48 -0700

    Structures Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,31 +13,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
+dog_t *new_dog;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+	return (NULL);
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	return (NULL);
 
-    new_dog->name = strdup(name);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+	free(new_dog);
+	return (NULL);
+	}
 
-    new_dog->age = age;
+	new_dog->age = age;
 
-    new_dog->owner = strdup(owner);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+	free(new_dog->name);
+	free(new_dog);
+	return (NULL);
+	}
 
-    return (new_dog);
+	return (new_dog);
 }
