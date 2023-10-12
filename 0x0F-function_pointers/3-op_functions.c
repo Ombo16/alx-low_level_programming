f4a9b6b606581ded060cd68a90ebeb07983ee5da
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-12 14:10:07 -0700

    Function Pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,50 @@
 /**
- * op_add - Returns the sum of a and b
- * @a: First integer
- * @b: Second integer
- * Return: Sum of a and b
+ * op_add - sum of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a + b
  */
-int op_add(int a, int b) {
-    return a + b;
+int op_add(int a, int b)
+{
+	return (a + b);
 }
-
 /**
- * op_sub - Returns the difference of a and b
- * @a: First integer
- * @b: Second integer
- * Return: Difference of a and b
+ * op_sub - substract of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a - b
  */
-int op_sub(int a, int b) {
-    return a - b;
+int op_sub(int a, int b)
+{
+	return (a - b);
 }
-
 /**
- * op_mul - Returns the product of a and b
- * @a: First integer
- * @b: Second integer
- * Return: Product of a and b
+ * op_mul - multiply of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a * b
  */
-int op_mul(int a, int b) {
-    return a * b;
+int op_mul(int a, int b)
+{
+	return (a * b);
 }
-
 /**
- * op_div - Returns the result of the division of a by b
- * @a: Dividend
- * @b: Divisor
- * Return: Result of a divided by b
+ * op_div - divide of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a / b
  */
-int op_div(int a, int b) {
-    return a / b;
+int op_div(int a, int b)
+{
+	return (a / b);
 }
-
 /**
- * op_mod - Returns the remainder of the division of a by b
- * @a: Dividend
- * @b: Divisor
- * Return: Remainder of a divided by b
+ * op_mod - module of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a % b
  */
-int op_mod(int a, int b) {
-    return a % b;
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
-
