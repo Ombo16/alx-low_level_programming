a514211b5e98e0c6a37c158a2b1d28ec71748a48
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-19 16:30:06 -0700

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * print_list - Print all the elements of a list_t list
- * @h: A pointer to the head of the list
- * Return: The number of nodes in the list
+ * struct list_s - singly linked list
+ * @str: string (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
+int _putchar(char c);
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the linked list.
+ * Return: The number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;  // Initialize the count of nodes to 0
+    size_t count = 0;
 
-    while (h != NULL) {
-        if (h->str == NULL) {
-            printf("[0] (nil)\n");
-        } else {
-            printf("[%d] %s\n", h->len, h->str);
+    while (h)
+    {
+        if (h->str == NULL)
+        {
+            int i = 0;
+            char nil[] = "(nil)\n";
+            while (nil[i] != '\0')
+            {
+                _putchar(nil[i]);
+                i++;
+            }
+        }
+        else
+        {
+            int i = 0;
+            while (h->str[i] != '\0')
+            {
+                _putchar(h->str[i]);
+                i++;
+            }
+            _putchar('\n');
         }
         count++;
         h = h->next;
