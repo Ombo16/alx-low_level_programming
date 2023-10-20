4307052db8c79d17f841d397b0121bb185b8e031
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-20 03:40:21 -0700

    singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: A pointer to a character string
- * Return: The length of the string.
- */
-int _strlen(const char *s)
-{
-	unsigned int len;
-
-	for (len = 0; s[len] != '\0'; len++)
-	{
-	}
-	return (len);
-
-}
-
-/**
- * add_node_end - Add a new node to the end of a linked list.
- * @head: A pointer to a pointer to the head of the list
- * @str: The string to be stored in the new node
- * Return: A pointer to the newly added node
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and added to the new node.
+ * Return: The address of the new element, or NULL if it fails.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *n_node, *tmp_n_d;
+	list_t *new_node;
+	list_t *temp;
 
-	n_node = malloc(sizeof(list_t));
-
-	if (n_node == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	n_node->str = strdup(str);
-	n_node->len = _strlen(str);
-	n_node->next = NULL;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-	if (!*head)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		*head = n_node;
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
 	}
 	else
 	{
-		tmp_n_d = *head;
-		while (tmp_n_d->next)
-			tmp_n_d = tmp_n_d->next;
-		{
-			tmp_n_d->next = n_node;
-		}
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
 	}
-	return (n_node);
+
+	return (new_node);
 }
