fa3dc5fffc380ba48861cc96aaf73ca0f6f83a56
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-20 03:55:57 -0700

    singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,26 @@
 section .data
-    hello db "Hello, Holberton",0
-    format db "%s", 10, 0  ; Format string with newline
+    hello db "Hello, Holberton,",10  ; The string to print with a newline character
+    hello_len equ $ - hello           ; Length of the string
 
 section .text
     global main
+
     extern printf
 
 main:
-    push hello
-    push format
-    call printf
+    sub rsp, 8                        ; Align the stack to 16 bytes for calling printf
+    mov rdi, hello                   ; The format string
+    call printf                      ; Call the printf function
+    add rsp, 8                        ; Restore the stack pointer
 
-    add rsp, 16  ; Restore the stack
+    mov rax, 60                      ; syscall number for exit (60 on x86_64)
+    xor rdi, rdi                     ; status (0)
+    syscall                          ; invoke syscall to exit
 
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit (60)
-    xor rdi, rdi        ; status = 0
-    syscall
+section .data
+    format db "%s", 0                 ; Null-terminated format string
+
+section .bss
+    dummy resb 16                     ; Space for alignment
+
 
