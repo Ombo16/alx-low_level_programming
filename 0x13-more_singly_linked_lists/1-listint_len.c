6be385b9d4ab7dd5ce322fd4a5f474025822990c
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-25 20:26:36 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - Return the number of elements in a listint_t list.
+ * @h: Pointer to the head of the list.
  *
- * Return: number of nodes
+ * Return: The number of elements in the list.
  */
-size_t listint_len(const listint_t *h)
-{
-size_t num = 0;
-while (h)
-{
-num++;
-h = h->next;
-}
-return (num);
+size_t listint_len(const listint_t *h) {
+    size_t count = 0;
+
+    while (h != NULL) {
+        h = h->next;
+        count++;
+    }
+
+    return count;
 }
