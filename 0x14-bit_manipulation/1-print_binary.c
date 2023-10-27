1c48b51c3b5982ed7488526270e415d7b7b7deb6
Ombo16 <ombo.mcboaz@gmail.com>
2023-10-27 14:42:10 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,27 +6,19 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
-    int shift;
-    int flag = 0;
-
-    for (shift = (sizeof(unsigned long int) * 8 - 1); shift >= 0; shift--)
-    {
-        unsigned long int mask = 1UL << shift;
-
-        if (n & mask)
-        {
-            _putchar('1');
-            flag = 1;
-        }
-        else if (flag)
-        {
-            _putchar('0');
-        }
-    }
+int i, count = 0;
+unsigned long int current;
+for (i = 63; i >= 0; i--)
+{
+current = n >> i;
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
 }
